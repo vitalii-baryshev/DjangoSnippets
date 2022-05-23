e33714414f9a63317f73933933d1ef160920170c
vitalii-baryshev <vbaryshev1991@gmail.com>
2022-05-23 21:44:22 +0300

    Исправлены urlы. Создан base.html

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -21,7 +21,7 @@ from MainApp import views
 
 urlpatterns = [
     path('', views.index_page),
-    path('snippets/add', views.add_snippet_page),
-    path('snippets/list', views.snippets_page),
+    path('/snippets/add', views.add_snippet_page, name="snippets-add"),
+    path('/snippets/list', views.snippets_page, name="snippets-list"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
