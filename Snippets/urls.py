49c052abb2436603bb84f4cca82b688872798d3c
vitalii-baryshev <vbaryshev1991@gmail.com>
2022-05-23 20:58:45 +0300

    Исправлены urlы и статика

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from MainApp import views
 
@@ -21,5 +23,5 @@ urlpatterns = [
     path('', views.index_page),
     path('snippets/add', views.add_snippet_page),
     path('snippets/list', views.snippets_page),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
